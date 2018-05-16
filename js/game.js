1dd6140f1ffb053aff9a0b8f008549f01d59468c
Gagan <hbti.gagan@gmail.com>
2018-05-16 23:51:22 +0530

    new assets

--- js/game.js
+++ js/game.js
@@ -15,6 +15,7 @@ DragonRun.Game.prototype = {
         
     },
     createBackground: function(){
+        alert('cloud added');
     	// this.bg = this.add.tileSprite(0, 0, this.game.world.width, this.game.world.height,'background');
     	this.cloud1 = this.add.sprite(0,0,'cloud1');
     }
--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 var DragonRun = DragonRun || {};
-DragonRun.game = new Phaser.Game(400, 300, Phaser.AUTO);
+DragonRun.game = new Phaser.Game(800, 600, Phaser.AUTO);
 DragonRun.game.state.add('Boot', DragonRun.Boot);
 DragonRun.game.state.add('Preload', DragonRun.Preload);
 DragonRun.game.state.add('Options', DragonRun.Options);
